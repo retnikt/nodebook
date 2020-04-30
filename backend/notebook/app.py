e605015d87a85a83179b4fac176178dbcd7230c6
retnikt <_@retnikt.uk>
2020-04-30 16:42:06 +0100

    return headers from HTTPException correctly

--- backend/notebook/app.py
+++ backend/notebook/app.py
@@ -14,11 +14,11 @@ app = API(docs_url=None, redoc_url=None, openapi_url="/api/openapi.json",)
 
 
 @app.exception_handler(HTTPException)
-async def http_exception_handler(_request, exception):
+async def http_exception_handler(_request, exception: HTTPException):
     """custom handler for HTTPExceptions allowing more flexibility:
     instead of returning { "detail": detail } we just return the detail as JSON
     """
-    return ORJSONResponse(exception.detail, status_code=exception.status_code)
+    return ORJSONResponse(exception.detail, status_code=exception.status_code, headers=exception.headers)
 
 
 app.add_api_route("/api/", app.redoc_route(), include_in_schema=False)
