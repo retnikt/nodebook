6db57ea234b919d0fae74c48f51005faedb96fdb
retnikt <_@retnikt.uk>
2020-05-02 17:09:05 +0100

    add users table definition

--- backend/notebook/database.py
+++ backend/notebook/database.py
@@ -4,8 +4,20 @@ This software is licensed under the MIT Licence: https://opensource.org/licenses
 """
 import sqlalchemy
 from databases import Database
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import text
 
 from notebook.settings import settings
 
 metadata = sqlalchemy.MetaData()
 database = Database(settings.dsn)
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column(
+        "id", UUID(True), primary_key=True, server_default=text("uuid_generate_v4()")
+    ),
+    sqlalchemy.Column("name", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("email", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("password", sqlalchemy.String, nullable=False),
+)
