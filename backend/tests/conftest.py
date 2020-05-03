10ea4ceae83bbb6fad0ec2679a590d3135c904ba
retnikt <_@retnikt.uk>
2020-05-03 20:48:23 +0100

    add sql fixture for tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 import pytest
@@ -12,3 +13,16 @@ def client():
 
     with starlette.testclient.TestClient(app) as client:
         yield client
+
+
+@pytest.fixture()
+def sql():
+    """fixture to execute arbitrary SQL statements"""
+    from notebook.database import database
+
+    loop = asyncio.get_event_loop()
+
+    def execute(query, **values):
+        return loop.run_until_complete(database.execute(query, values or None))
+
+    return execute
