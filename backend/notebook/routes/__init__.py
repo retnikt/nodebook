f72f0f46b4cd1d70efe7da11f777df916c8ea950
retnikt <_@retnikt.uk>
2020-05-09 18:31:21 +0100

    add profile routes

--- backend/notebook/routes/__init__.py
+++ backend/notebook/routes/__init__.py
@@ -5,6 +5,8 @@ This software is licensed under the MIT Licence: https://opensource.org/licenses
 from fastapi import APIRouter
 
 from .oauth2 import router as auth
+from .profile import router as profile
 
 router = APIRouter()
 router.include_router(auth, prefix="/oauth2")
+router.include_router(profile, prefix="/profile")
---
+++ backend/notebook/routes/profile.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+import pydantic
+from fastapi import APIRouter
+from pydantic import EmailStr
+
+from notebook import database
+from notebook.controllers.oauth2 import requires
+from notebook.utils import Ok
+
+router = APIRouter()
+
+
+class Profile(pydantic.BaseModel):
+    email: str
+    name: str
+
+
+@router.get("/", response_model=Profile)
+async def get_profile(user: requires("user/read")):
+    """gets the user's profile"""
+    return {
+        "email": user["email"],
+        "name": user["name"],
+    }
+
+
+@router.put("/", response_model=Ok)
+async def update_profile(
+    user: requires("user/read", "user/write"),
+    email: Optional[EmailStr] = None,
+    name: Optional[str] = None,
+):
+    """updates the user's profile"""
+    data = {}
+    if email is not None:
+        data["email"] = email
+    if name is not None:
+        data["name"] = name
+    # if there is nothing to update, don't bother
+    if data:
+        await database.database.execute(
+            (
+                database.users.update()
+                .where(database.users.c.id == user["id"])
+                .values(data)
+            )
+        )
+    return "ok"
+
+
+@router.delete("/", response_model=Ok)
+async def delete_profile(user: requires("user/read", "user/write", "user/delete")):
+    """deletes a user account"""
+    await database.database.execute(
+        database.users.delete().where(database.users.c.id == user["id"])
+    )
+    return "ok"
